845e55043265690213d0d7ef8f333c1219db99c8
caughmanconsulting <caughmanconsulting@gmail.com>
2023-02-01 14:50:08 -0500

    menu update

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,5 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-// import menu_light from "../images/menu_light.png";
-// import menu_dark from "../images/menu_dark.png";
 import menu_bg from "../images/menu_bg.svg";
 import logo_light from "../images/logo_light.png";
 import logo_dark from "../images/logo_dark.png";
@@ -9,8 +7,6 @@ import gem from "../images/gem.svg";
 import twitter from "../images/twitter.svg";
 import discord from "../images/discord.svg";
 import opensea from "../images/opensea.svg";
-import { FaTwitter, FaDiscord } from "react-icons/fa";
-import { GiSailboat } from "react-icons/gi";
 import { themeContext } from "../App";
 import themes from "../helpers/themes";
 
@@ -25,85 +21,96 @@ const Menu = () => {
     >
       <div className="offcanvas-body dark:bg-[#000000]">
         <div className="flex flex-row h-full overflow-hidden">
-          <div className="basis-3/4 sm:basis-1/2 flex relative">
-            <img src={menu_bg} className="bg-fixed scale-150" />
-            <div className="absolute h-full w-full">
-              <ul className="menu-links h-full flex flex-col justify-evenly items-center xl:space-y-10 p-3">
-                <li className="flex">
-                  <Link
-                    to="/home"
-                    data-bs-dismiss="offcanvas"
-                    className="menu-link flex items-start text-4xl sm:text-5xl lg:text-7xl 2xl:text-8xl"
-                  >
-                    <span className="text-black dark:text-white primary uppercase">
-                      Home
-                    </span>
-                    <span className="text-black dark:text-white secondary uppercase">
-                      Home
-                    </span>
-                  </Link>
-                </li>
-                <li className="flex">
-                  <Link
-                    to="/utopia"
-                    data-bs-dismiss="offcanvas"
-                    className="dark:text-white menu-link flex items-start text-4xl sm:text-5xl lg:text-7xl 2xl:text-8xl"
-                  >
-                    <span className="text-black dark:text-white primary uppercase">
-                      Utopia
-                    </span>
-                    <span className="text-black dark:text-white secondary uppercase">
-                      Utopia
-                    </span>
-                  </Link>
-                </li>
-                <li className="flex">
-                  <Link
-                    to="/story"
-                    data-bs-dismiss="offcanvas"
-                    className="dark:text-white menu-link flex items-start text-4xl sm:text-5xl lg:text-7xl 2xl:text-8xl"
-                  >
-                    <span className="text-black dark:text-white primary uppercase">
-                      Story
-                    </span>
-                    <span className="text-black dark:text-white secondary uppercase">
-                      Story
-                    </span>
-                  </Link>
-                </li>
-                <li className="flex">
-                  <Link
-                    to="/staking"
-                    data-bs-dismiss="offcanvas"
-                    className="dark:text-white menu-link flex items-start text-4xl sm:text-5xl lg:text-7xl 2xl:text-8xl"
-                  >
-                    <span className="text-black dark:text-white primary uppercase">
-                      Staking
-                    </span>
-                    <span className="text-black dark:text-white secondary uppercase">
-                      Staking
-                    </span>
-                  </Link>
-                </li>
-                <li className="flex">
-                  <Link
-                    to="/raffle"
-                    data-bs-dismiss="offcanvas"
-                    className="dark:text-white menu-link flex items-start text-4xl sm:text-5xl lg:text-7xl 2xl:text-8xl"
-                  >
-                    <span className="text-black dark:text-white primary uppercase">
-                      Raffle
-                    </span>
-                    <span className="text-black dark:text-white secondary uppercase">
-                      Raffle
-                    </span>
-                  </Link>
-                </li>
-              </ul>
+          <div className="basis-3/4 sm:basis-2/5 flex relative">
+            <img src={menu_bg} className="menu-bg2" />
+            <div className="absolute h-full w-full h-full">
+              <div className="flex flex-row visibile sm:invisible mt-2">
+                <div className="basis-1/2 ml-3 flex justify-end">
+                  <img
+                    src={theme == themes.DARK ? logo_light : logo_dark}
+                    className="h-36 visibile sm:invisible"
+                    alt="logo"
+                  />
+                </div>
+              </div>
+              <div className="flex flex-col h-3/4 justify-start sm:justify-center items-center ml-8 text-justify mt-14 sm:mt-0">
+                <ul className="menu-links space-y-5 p-0">
+                  <li className="flex">
+                    <Link
+                      to="/home"
+                      data-bs-dismiss="offcanvas"
+                      className="menu-link flex items-start text-4xl sm:text-4xl lg:text-6xl 2xl:text-7xl"
+                    >
+                      <span className="text-black dark:text-white primary uppercase">
+                        Home
+                      </span>
+                      <span className="text-black dark:text-white secondary uppercase">
+                        Home
+                      </span>
+                    </Link>
+                  </li>
+                  <li className="flex">
+                    <Link
+                      to="/utopia"
+                      data-bs-dismiss="offcanvas"
+                      className="menu-link flex items-start text-4xl sm:text-4xl lg:text-6xl 2xl:text-7xl"
+                    >
+                      <span className="text-black dark:text-white primary uppercase">
+                        Utopia
+                      </span>
+                      <span className="text-black dark:text-white secondary uppercase">
+                        Utopia
+                      </span>
+                    </Link>
+                  </li>
+                  <li className="flex">
+                    <Link
+                      to="/story"
+                      data-bs-dismiss="offcanvas"
+                      className="menu-link flex items-start text-4xl sm:text-4xl lg:text-6xl 2xl:text-7xl"
+                    >
+                      <span className="text-black dark:text-white primary uppercase">
+                        Story
+                      </span>
+                      <span className="text-black dark:text-white secondary uppercase">
+                        Story
+                      </span>
+                    </Link>
+                  </li>
+                  <li className="flex">
+                    <Link
+                      to="/staking"
+                      data-bs-dismiss="offcanvas"
+                      className="menu-link flex items-start text-4xl sm:text-4xl lg:text-6xl 2xl:text-7xl"
+                    >
+                      <span className="text-black dark:text-white primary uppercase">
+                        Staking
+                      </span>
+                      <span className="text-black dark:text-white secondary uppercase">
+                        Staking
+                      </span>
+                    </Link>
+                  </li>
+                  <li className="flex">
+                    <Link
+                      to="/raffle"
+                      data-bs-dismiss="offcanvas"
+                      className="menu-link flex items-start text-4xl sm:text-4xl lg:text-6xl 2xl:text-7xl"
+                    >
+                      <span className="text-black dark:text-white primary uppercase">
+                        Raffle
+                      </span>
+                      <span className="text-black dark:text-white secondary uppercase">
+                        Raffle
+                      </span>
+                    </Link>
+                  </li>
+                </ul>
+              </div>
             </div>
           </div>
-          <div className="basis-0 sm:basis-2/6">
-            <div className="h-full flex justify-center items-center p-2 lg:p-10">
+          <div className="basis-0 sm:basis-2/5">
+            <div className="h-full flex justify-center items-center p-2">
               <img
                 src={theme == themes.DARK ? logo_light : logo_dark}
                 className="bg-center"
@@ -111,7 +118,7 @@ const Menu = () => {
               />
             </div>
           </div>
-          <div className="basis-1/4 sm:basis-1/6 grow bg-purple z-10 overflow-hidden">
+          <div className="basis-1/4 sm:basis-1/5 grow bg-purple z-10 overflow-hidden">
             <div className="flex flex-col h-full">
               <div className="basis-1/4 flex justify-center items-center">
                 <a data-bs-dismiss="offcanvas">
@@ -122,9 +129,9 @@ const Menu = () => {
                   ></img>
                 </a>
               </div>
-              <div className="basis-3/4 flex flex-col justify-evenly items-center content-center p-3 sm:p-2">
+              <div className="basis-3/4 flex flex-col justify-center items-center content-center p-3 sm:p-2">
                 <a
-                  className="social-icon sm:p-5 hover:scale-75"
+                  className="social-icon sm:p-5 scale-75 hover:scale-100"
                   target="_blank"
                   rel="noreferrer"
                   href="https://twitter.com/zenogakki?s=11&t=0yRkX8FJTYUdvCdP1SzhHg"
@@ -133,7 +140,7 @@ const Menu = () => {
                   {/* <FaTwitter className="dark:text-black drop-shadow-lg bg-white/[.3] rounded p-1" /> */}
                 </a>
                 <a
-                  className="social-icon sm:p-5 hover:scale-75"
+                  className="social-icon sm:p-5 scale-75 hover:scale-100"
                   target="_blank"
                   rel="noreferrer"
                   href="https://discord.com/invite/projectzenogakki"
@@ -142,7 +149,7 @@ const Menu = () => {
                   {/* <FaDiscord className="dark:text-black drop-shadow-lg bg-white/[.3] rounded p-1" /> */}
                 </a>
                 <a
-                  className="social-icon sm:p-5 hover:scale-75"
+                  className="social-icon sm:p-5 scale-75 hover:scale-100"
                   target="_blank"
                   rel="noreferrer"
                   href="https://t.co/ruxXGaXmer"
